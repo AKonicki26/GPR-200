6df78208d0aaeb33a72cbbd3c20bc5861e617678
Anne <112440960+AKonicki26@users.noreply.github.com>
2024-09-13 11:13:58 -0400

    Assignment 1 end result

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -12,17 +12,33 @@ const int SCREEN_HEIGHT = 720;
 const char *vertexShaderSource = R"(
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
+out vec4 Color; // Varying
+
+uniform float uTime;
+
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    Color = aColor; // Pass-through
+    vec3 pos = aPos;
+    pos.y += sin((2 * uTime) - pos.x) / 4.0;
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }
 )";
 const char *fragmentShaderSource = R"(
 #version 330 core
-out vec4 FragColor;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
+out vec4 Color; // Varying
+
+uniform float uTime;
+
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    Color = aColor; // Pass-through
+    vec3 pos = aPos;
+    pos.y += sin((2 * uTime) - pos.x) / 4.0;
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }
 )";
 
@@ -45,40 +61,44 @@ int main() {
 
     //Initialization goes here!
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f, 1.0, 0.0, 0.0, 1.0,
+        0.5f, -0.5f, 0.0f, 0.0, 1.0, 0.0, 1.0,
+        0.0f, 0.5f, 0.0f, 0.0, 0.0, 1.0, 1.0
     };
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
 
+    int success;
+    char infoLog[512];
+
+    // Create and compile vertex shader
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
-
-    int success;
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success) {
-        char infoLog[512];
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         printf("%s\n", infoLog);
     }
 
+    // Create and compile fragment shader
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
     if (!success) {
-        char infoLog[512];
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         printf("%s\n", infoLog);
     }
 
+    // Create shader program and link shaders
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
@@ -89,8 +109,11 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
-    glEnableVertexAttribArray(0);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("%s\n", infoLog);
+    }
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -98,20 +121,31 @@ int main() {
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+
+    // Position (XYZ)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-
+    // Color (RGBA)
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     //Render loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        float time = (float)glfwGetTime();
         //Clear framebuffer
         glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         //Drawing happens here!
+        glUseProgram(shaderProgram);
+
+        int timeLoc = glGetUniformLocation(shaderProgram, "uTime");
+        glUniform1f(timeLoc, time);
+
+        glBindVertexArray(VAO);
+
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
     }

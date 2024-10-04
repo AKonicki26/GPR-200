1b3bcba1e295e6a15352ac125b97f6ffd9d574cb
Anne <112440960+AKonicki26@users.noreply.github.com>
2024-10-04 10:29:10 -0400

    Add Camera Class

---
+++ core/ak/Camera.cpp
@@ -0,0 +1,8 @@
+//
+// Created by anne.konicki on 10/4/2024.
+//
+
+#include "Camera.h"
+
+namespace ak {
+} // ak
---
+++ core/ak/Camera.h
@@ -0,0 +1,16 @@
+//
+// Created by anne.konicki on 10/4/2024.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+namespace ak {
+
+class Camera {
+
+};
+
+} // ak
+
+#endif //CAMERA_H
